9ce8a46dd073dfe1332fe34642d43080393e9999
quisiecaporte <kredoteacher.qcaporte@gmail.com>
2025-06-09 15:58:49 +0800

    Update index.php

--- views/index.php
+++ views/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+<!--    this is body content -->
     <div class="container mt-5">
         <div class="card border-0 mx-auto w-50">
             <div class="card-header border-0 bg-white mt-5">
